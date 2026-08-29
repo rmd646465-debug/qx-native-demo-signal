38e25f78658376791da5ed2a5a2100b9d01d4810
rmd646465-debug <rmd646465@gmail.com>
2026-08-29 11:09:30 +0600

    Fix recent-candle balance in horizon mirror test

--- engine-test/com/qx/strictsignal/SignalEngineSelfTest.java
+++ engine-test/com/qx/strictsignal/SignalEngineSelfTest.java
@@ -42,7 +42,7 @@ public final class SignalEngineSelfTest {
         for (int i = 0; i < 42; i++) {
             float delta;
             if (i < 28) delta = (i % 2 == 0 ? 4f : -0.5f) * direction;
-            else delta = (i % 2 == 0 ? 1f : -0.5f) * direction;
+            else delta = (i % 2 == 1 ? 1f : -0.5f) * direction;
             price += delta;
             candles.add(new SignalEngine.CandlePoint(i * 12f, price, 2f, delta > 0));
         }
